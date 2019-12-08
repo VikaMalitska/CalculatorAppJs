7cfb6d3900ae70e9031cafd9acc3d110dc236ccd
Viktoriia Malitska <viktoriia.malitska@gmail.com>
2019-12-08 23:43:52 +0200

    calculation

--- calculator.js
+++ calculator.js
@@ -1,10 +1,27 @@
  class Calculator{
-    constructor(){
-        this.a = 0; 
-        this.b = 0;
-    }
     sum (a=0,b=0){
-        return Num(a + b);
+        return  Number(a) + Number(b);
+    }
+
+    mul(a=0,b=0) {
+        return Number(a) * Number(b);
+    }
+
+    div(a=0, b=0) {
+        return a / b;
+    }
+
+    sub(a=0, b=0) {
+      return a - b;
     }
 }
-export {Calculator};
+
+class Display {
+    constructor(){
+        this.a = 0;
+        this.b = 0;
+        this.operator = "";
+        this.isenabled = false;
+    }
+}
+export {Calculator, Display};
--- index.js
+++ index.js
@@ -1,28 +1,102 @@
-//import {Calculator} from './calculator';
+import { Calculator, Display } from "./calculator.js";
 
-//const calc = new Calculator();
+const calc = new Calculator();
 
 let elemNumberBut = document.querySelectorAll(".key-num-btn");
 let elemInput = document.querySelector(".display");
 let elemDecimal = document.querySelector(".key-decimal");
 let elemAC = document.querySelector(".key-clear");
+let elemOperator = document.querySelectorAll(".key-operator-btn");
+let elemEqual = document.querySelector(".key-equal");
 
 let keyNum = [...elemNumberBut];
+let keyOper = [...elemOperator];
 
-function eventHandlerKeyNum(a) {
-    elemInput.value  = Number(elemInput.value + a.target.value);
-    console.log(elemInput.value);
+let display;
+
+function eventHandlerKeyNum(e) {
+    if (display) {
+        elemInput.value = elemInput.value + e.target.value;
+        if (display.operator === "") {
+            display.a = elemInput.value;
+        } else {
+            if (display.b == 0) {
+                display.b = Number(e.target.value);
+            } else {
+                display.b = display.b + Number(e.target.value);
+            }
+        }
+    } else {
+        display = new Display();
+        elemInput.value = e.target.value;
+        display.isenabled = true;
+        display.a = elemInput.value;
+    }
 }
-function eventHandlerDecimal(){
-    elemInput.value = elemInput.value + ".";
+
+function eventHandlerOperator(a) {
+    if (!!display.b) {
+        display.b = 0;
+        display.a = elemInput.value;
+    }
+    display.operator = a.target.value;
+    elemInput.value = elemInput.value + a.target.value;
 }
+
+function eventHandlerDecimal(e) {
+    elemInput.value = elemInput.value + e.target.value;
+    if (display) {
+        if (display.operator === "") {
+            display.a = display.a + e.target.value;
+        } else {
+            display.b = display.b + e.target.value;
+        }
+    }
+}
+
+function eventHandlerEqual() {
+    console.log(display);
+    elemInput.value = calculation().toFixed(4);
+    //console.log(display);
+    if (!!display.operator || !!display.b || !!display.a) {
+        return;
+    }
+    display = false;
+}
+
 function eventHandlerAC() {
     elemInput.value = 0;
+    display = false;
 }
 
-keyNum.forEach(element => {
-    element.addEventListener('click', eventHandlerKeyNum);
-});
-elemDecimal.addEventListener('click', eventHandlerDecimal);
+function calculation() {
+    let res;
+    switch (display.operator) {
+        case "+":
+            res = calc.sum(display.a, display.b);
+            break;
+        case "-":
+            res = calc.sub(display.a, display.b);
+            break;
+        case "*":
+            res = calc.mul(display.a, display.b);
+            break;
+        case "/":
+            res = calc.div(display.a, display.b);
+            break;
+    }
+    return res;
+}
 
-elemAC.addEventListener('click', eventHandlerAC);
+keyOper.forEach(element => {
+    element.addEventListener("click", eventHandlerOperator);
+});
+
+keyNum.forEach(element => {
+    element.addEventListener("click", eventHandlerKeyNum);
+});
+elemDecimal.addEventListener("click", eventHandlerDecimal);
+
+elemAC.addEventListener("click", eventHandlerAC);
+
+elemEqual.addEventListener("click", eventHandlerEqual);
